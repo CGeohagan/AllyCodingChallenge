a88816919d1daa86b41802d14a59af5342549df8
Colleen Geohagan <ckbowker@gmail.com>
2017-07-09 21:07:26 -0400

    Adding in functionality for sidebar toggle buttons

--- assets/development/js/app.js
+++ assets/development/js/app.js
@@ -1,18 +1,44 @@
 jQuery(document).ready(function($){
-// script goes here
+// Scripts goes here
 	
+
+
 /* JSON File Scripts */
 
 // Use JSON File to make a rates table list
 
-$.getJSON( "assets/js/code-test.json", function( data ) {
-  var ratesObject = JSON.stringify(data);
+// $.getJSON( "assets/js/code-test.json", function( data ) {
+//   var ratesObject = JSON.stringify(data);
+// });
+
+// console.log(ratesObject);
+
+
+
+/* Sidebar Toggle Scripts */
+
+var sidebar = document.querySelector('.sidebar');
+var sidebarButtons = document.querySelectorAll('.sidebar__button');
+var sidebarSections = document.querySelectorAll('.sidebar__section');
+
+// Add click listener to the sidebar
+sidebar.addEventListener("click", function(e) {
+	// e.target is the clicked element
+	// If the clicked item contains the sidebar button class
+	if (e.target && e.target.classList.contains('sidebar__button')) {
+
+		// Toggle the active class for the sidebar buttons
+		sidebarButtons.forEach(function(item){
+			item.classList.toggle('active');
+		});
+
+		// Toggle the active class for the sidebar sections
+		sidebarSections.forEach(function(item){
+			item.classList.toggle('active');
+		});
+		
+	}			
 });
-
-console.log(ratesObject);
-
-
-
 
 
 	
